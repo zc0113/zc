5db2f2332824cd7a6c0142a318b5827813cf39be
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-03-02 16:06:26 +0800

    修改20，155

--- 155zc.c
+++ 155zc.c
@@ -1,5 +1,4 @@
-typedef struct
-{
+typedef struct{
     int capacity;
     int topofstack;
     int *array;
@@ -7,56 +6,42 @@ typedef struct
 
 /** initialize your data structure here. */
 #define EMPTYTOS (-1)
-MinStack* minStackCreate(int maxSize)
-{
+MinStack* minStackCreate(int maxSize){
     MinStack *S;
-    S = malloc(sizeof(MinStack)*maxSize);
-    if(S == NULL)
-        return NULL;
+    S = malloc(sizeof(MinStack));
+    if (S == NULL) return NULL;
     S->array = malloc(sizeof(int)*maxSize);
-    if(S->array == NULL)
-        return NULL;
+    if (S->array == NULL) return NULL;
     S->topofstack = EMPTYTOS;
     S->capacity = maxSize;
     return S;
 }
 
-void minStackPush(MinStack* obj, int x)
-{
-    if(obj->topofstack == obj->capacity)
-        return NULL;
-    else
-        obj->array[++obj->topofstack]=x;
+void minStackPush(MinStack* obj, int x){
+    if (obj->topofstack == obj->capacity) return NULL;
+    obj->array[++obj->topofstack]=x;
 }
 
-void minStackPop(MinStack* obj)
-{
-    if(obj->topofstack == EMPTYTOS)
-        return NULL;
-    else
-        obj->topofstack--;
+void minStackPop(MinStack* obj){
+    if (obj->topofstack == EMPTYTOS) return NULL;
+    obj->topofstack--;
 }
 
-int minStackTop(MinStack* obj)
-{
-    if( (obj->topofstack!=EMPTYTOS) )
+int minStackTop(MinStack* obj){
+    if ((obj->topofstack!=EMPTYTOS))
         return obj->array[obj->topofstack];
     return 0;
 }
 
-int minStackGetMin(MinStack* obj)
-{
+int minStackGetMin(MinStack* obj){
     int min = obj->array[obj->topofstack];
-    int tmp = 0;
-    for(int i=obj->topofstack;i>EMPTYTOS;i--)
-    {
-        if( obj->array[i] < min)
+    for (int i = obj->topofstack; i > EMPTYTOS; i--) {
+        if (obj->array[i] < min)
             min = obj->array[i];
     }
     return min;
 }
 
-void minStackFree(MinStack* obj)
-{
-    free(obj);
+void minStackFree(MinStack* obj){
+    if (obj) free(obj);
 }
