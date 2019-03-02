36ea93c6ee5901ade26a0218fd4cc8becdb0fa6c
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-03-02 16:16:34 +0800

    修改232

--- 232zc.c
+++ 232zc.c
@@ -1,12 +1,10 @@
-typedef struct
-{
+typedef struct {
     int *arr;
     int top;
 } MyQueue;
 
 /** Initialize your data structure here. */
-MyQueue* myQueueCreate(int maxSize)
-{
+MyQueue* myQueueCreate(int maxSize) {
     MyQueue *queue = (MyQueue *)malloc(sizeof(MyQueue));
     queue->arr = (int *)malloc(sizeof(int) * maxSize);
     queue->top = -1;
@@ -14,18 +12,13 @@ MyQueue* myQueueCreate(int maxSize)
 }
 
 /** Push element x to the back of queue. */
-void myQueuePush(MyQueue* obj, int x)
-{
+void myQueuePush(MyQueue* obj, int x) {
     int tmp = obj->top;
-    if(obj->top == -1)
-    {
+    if (obj->top == -1) {
         obj->arr[0] = x;
         obj->top++;
-    }
-    else
-    {
-        while(obj->top != -1)
-        {
+    } else {
+        while(obj->top != -1) {
             obj->arr[obj->top+1] = obj->arr[obj->top];
             obj->top--;
         }
@@ -35,32 +28,24 @@ void myQueuePush(MyQueue* obj, int x)
 }
 
 /** Removes the element from in front of queue and returns that element. */
-int myQueuePop(MyQueue* obj)
-{
-    if(-1 == obj->top)
-    {
+int myQueuePop(MyQueue* obj) {
+    if (-1 == obj->top)
         return 0;
-    }
     return obj->arr[obj->top--];
 }
 
 /** Get the front element. */
-int myQueuePeek(MyQueue* obj)
-{
-    if(obj->top == -1)
-    {
+int myQueuePeek(MyQueue* obj) {
+    if (obj->top == -1)
         return 0;
-    }
     return obj->arr[obj->top];
 }
 
 /** Returns whether the queue is empty. */
-bool myQueueEmpty(MyQueue* obj)
-{
+bool myQueueEmpty(MyQueue* obj) {
     return obj->top == -1 ? 1 : 0;
 }
 
-void myQueueFree(MyQueue* obj)
-{
+void myQueueFree(MyQueue* obj) {
     obj->top = -1;
 }
