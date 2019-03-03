fdaf658b448242efa6fb33f87543f67a6124bdd9
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-03-03 21:22:35 +0800

    格式规范

--- 496zc.c
+++ 496zc.c
@@ -1,17 +1,12 @@
-int* nextGreaterElement(int* findNums, int findNumsSize, int* nums, int numsSize, int* returnSize)
-{
+int* nextGreaterElement(int* findNums, int findNumsSize, int* nums, int numsSize, int* returnSize){
     int* a = (int*)malloc(sizeof(int) * findNumsSize);
     memset(a,-1,findNumsSize * sizeof(int));
     *returnSize = findNumsSize;
-    for(int i = 0;i < findNumsSize;i++)
-    {
+    for (int i = 0; i < findNumsSize; i++) {
         int mark = 0;
-        for(int j = 0;j < numsSize;j++)
-        {
-            if(findNums[i] == nums[j])
-                mark = 1;
-            if(findNums[i] < nums[j] && mark == 1)
-            {
+        for (int j = 0;j < numsSize;j++) {
+            if (findNums[i] == nums[j]) mark = 1;
+            if (findNums[i] < nums[j] && mark == 1) {
                 a[i] = nums[j];
                 break;
             }
